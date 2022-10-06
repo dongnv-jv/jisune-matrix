036a1fdfccef77e655756664342ea89c001a434a
Nguyen Van Dong <107174526+dongbk97@users.noreply.github.com>
2022-10-06 21:08:47 +0700

    Update dong.js

--- dong.js
+++ dong.js
@@ -21,7 +21,7 @@ class MatrixAnimation {
     }
 
     drawAnimation() {
-        let code = '012345678901010123456789010101'
+        let code = '0 1 2 3 4 5 6 7 8 9 0 1 0 1 0 1 2 3 4 5 6 7 8 9 0 1 0 1 0 1'
             
         if (this.status) {
             this.ctx.fillStyle = this.lettersColor;
